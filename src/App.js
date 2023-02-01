2a36449ca73f3e9dbee0d8dec6d7b40886c2fcf5
Sharat Rokkam <94468010+SharatRokkam@users.noreply.github.com>
2023-02-01 18:51:29 +0530

    basic-UI

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MastheadSlider from './Components/MastheadSlider'
+import MovieSlider from './Components/MovieSlider';
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <MastheadSlider />
+    <MovieSlider />
+ 
+    
+    </>
+  );
+}
+
+export default App;
